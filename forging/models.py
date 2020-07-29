41abc98cbdb29b737ae1535f7e337635a14926a1
42chameleon <42israpilov@mail.ru>
2020-07-29 23:09:01 +0300

    connect app and create model Forging, Category

--- forging/models.py
+++ forging/models.py
@@ -1,1 +1,13 @@
+
 from django.db import models
+
+
+class Forging(models.Model):
+    title = models.CharField('Заголовок', max_length=150)
+    content = models.TextField('Описание')
+    photo = models.ImageField('Фото', upload_to='media/')
+
+
+class Category(models.Model):
+    title = models.CharField('Заголовок', max_length=150)
+    telephone = models.CharField('Телефон сварщика', max_lengt=20)
