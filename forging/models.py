8a8d4b24d3a236061889eceefbe65f877abbac02
42chameleon <42israpilov@mail.ru>
2020-07-30 17:00:25 +0300

    wrote a path for storing photo, created the HomeView class, created a base template and inherited from it in the home_forgin_list template

--- forging/models.py
+++ forging/models.py
@@ -5,9 +5,10 @@ from django.db import models
 class Forging(models.Model):
     title = models.CharField('Заголовок', max_length=150)
     content = models.TextField('Описание')
+    is_published = models.BooleanField('Опубликовать', default=False, blank=True)
     photo = models.ImageField('Фото', upload_to='media/')
 
 
 class Category(models.Model):
     title = models.CharField('Заголовок', max_length=150)
-    telephone = models.CharField('Телефон сварщика', max_lengt=20)
+    telephone = models.CharField('Телефон сварщика', max_length=20)
--- forging/views.py
+++ forging/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Forging, Category
 
-# Create your views here.
+
+class HomeView(ListView):
+    model = Forging
+    template_name = 'forging/home_forging_list.html'
+    context_object_name = 'forging'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return Forging.objects.filter(is_published=True).select_related('category')
